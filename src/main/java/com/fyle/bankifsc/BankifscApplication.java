bd5b50bb883931ee8f2b5bfada3808399fb5f950
rajats081 <rjt.ks96@gmail.com>
2019-06-23 14:00:27 +0530

    INITAL PROJECT SETUP

---
+++ src/main/java/com/fyle/bankifsc/BankifscApplication.java
@@ -0,0 +1,13 @@
+package com.fyle.bankifsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankifscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankifscApplication.class, args);
+	}
+
+}
